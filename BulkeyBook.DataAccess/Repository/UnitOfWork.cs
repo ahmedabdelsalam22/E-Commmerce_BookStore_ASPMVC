a4e137429e5a5f63c8b248470ecfa90c6d5d13cf
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-15 12:41:33 +0200

    apply unitOfWork successfully

---
+++ BulkeyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            categoryRepository = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository categoryRepository { get; private set; }
+
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
