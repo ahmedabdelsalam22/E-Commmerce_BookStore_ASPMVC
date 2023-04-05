ab570f1e8cd984e1884b9f8fb04635db13316de6
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-04-05 15:31:07 +0200

    add new migration for ApplicationUser with new features

---
+++ BulkyBook.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public String Name { get; set; }
+        public String? StreetAddress { get; set; }
+        public String? City { get; set; }
+        public String? State { get; set; }
+        public String? PostalCode { get; set; }
+
+    }
+}
