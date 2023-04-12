1593a96b8c7c23e92a19498a3f97f8d288ab2dea
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-04-13 00:54:32 +0200

    Details_PAY_NOW() in OrderController

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Stripe;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
@@ -35,7 +36,59 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View(OrderVM);
         }
 
-        [HttpPost]
+		[ActionName("Details")]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Details_PAY_NOW() 
+		{
+			OrderVM.OrderHeader = _unitOfWork.orderHeaderRepository.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, includeProperties: "ApplicationUser");
+			OrderVM.OrderDetail = _unitOfWork.orderDetailRepository.GetAll(u => u.OrderId == OrderVM.OrderHeader.Id, includeProperties: "Product");
+
+			// STRIPE 
+			var domain = "https://localhost:44325/";
+			var options = new SessionCreateOptions
+			{
+				PaymentMethodTypes = new List<string>
+				{
+				  "card",
+				},
+				LineItems = new List<SessionLineItemOptions>(),
+				Mode = "payment",
+				SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderid={OrderVM.OrderHeader.Id}",
+				CancelUrl = domain + $"admin/order/details?orderId={OrderVM.OrderHeader.Id}",
+			};
+
+			foreach (var item in OrderVM.OrderDetail)
+			{
+
+				var sessionLineItem = new SessionLineItemOptions
+				{
+					PriceData = new SessionLineItemPriceDataOptions
+					{
+						UnitAmount = (long)(item.Price * 100),//20.00 -> 2000
+						Currency = "usd",
+						ProductData = new SessionLineItemPriceDataProductDataOptions
+						{
+							Name = item.Product.Title
+						},
+
+					},
+					Quantity = item.Count,
+				};
+				options.LineItems.Add(sessionLineItem);
+
+			}
+
+			var service = new SessionService();
+			Session session = service.Create(options);
+			_unitOfWork.orderHeaderRepository.UpdateStripePaymentID(OrderVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+			_unitOfWork.Save();
+			Response.Headers.Add("Location", session.Url);
+			return new StatusCodeResult(303);
+		}
+
+
+		[HttpPost]
         [ValidateAntiForgeryToken]
 		[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
 		public IActionResult UpdateOrderDetail()
